845141a8b2f4152dd0af901143b6ec7d25ebc71a
rudan <rudanr11111@gmail.com>
2025-10-17 19:20:04 +0900

    레벨 2 1.사칙연산 메서드 구현 및 컬렉션타입 필드 2.레벨 1 에서 구현한 계산기가 calculator 클래스가 활용되도록 수정

---
+++ src/calculatorlevel2/Calculator.java
@@ -0,0 +1,31 @@
+package calculatorlevel2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> number = new ArrayList<>();
+    }
+    public int calculate(int intBox1, int intBox2, char charBox1) {
+        if (charBox1 == '+') {
+            int resultSum = intBox1 + intBox2;
+            return resultSum;
+        } else if (charBox1 == '-') {
+            int resultSub = intBox1 - intBox2;
+            return resultSub;
+        }  else if (charBox1 == '*') {
+            int resultMult = intBox1 * intBox2;
+            return resultMult;
+        }  else if (charBox1 == '/') {
+            try {
+                int  divide = intBox1 / intBox2;
+                System.out.println("나눈셈 결과값 = " + divide);
+            } catch (ArithmeticException e) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/calculatorlevel2/Main.java
@@ -0,0 +1,35 @@
+package calculatorlevel2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        for(;;) {
+
+            //양의 정수 입력 받기
+            System.out.print("첫번째 양의 정수를 입력하세요: ");
+            int intBox1 = scanner.nextInt();
+            System.out.print("두번째 양의 정수를 입력하세요: ");
+            int intBox2 = scanner.nextInt();
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            char  charBox = scanner.next().charAt(0);
+
+            //사칙연산 +-*/
+            int result = calculator.calculate(intBox1, intBox2, charBox);
+            System.out.println("result = " + result);
+
+            // 탈출
+            System.out.println("더 계산하시려면 아무값이나 입력해주세요. (exit 입력 시 종료)");
+            String string = scanner.next();
+            if(string.equals("exit")) {
+                break;
+            }
+        }
+    }
+
+
+}
+
